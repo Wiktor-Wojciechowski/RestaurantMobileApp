b7e59862d9e9768879c505c42cb1c4376b3aef77
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-04-04 17:20:49 +0200

    moved models to a model package 2

--- app/src/main/java/com/example/restaurantapp/data/RestaurantRepository.kt
+++ app/src/main/java/com/example/restaurantapp/data/RestaurantRepository.kt
@@ -1,8 +1,8 @@
 package com.example.restaurantapp.data
 
-import com.example.restaurantapp.network.Dish
-import com.example.restaurantapp.network.LoginParams
-import com.example.restaurantapp.network.RegisterParams
+import com.example.restaurantapp.model.Dish
+import com.example.restaurantapp.model.LoginParams
+import com.example.restaurantapp.model.RegisterParams
 import com.example.restaurantapp.network.RestaurantApiService
 import okhttp3.ResponseBody
 import retrofit2.Call
