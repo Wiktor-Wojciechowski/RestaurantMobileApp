504a388eabaf8d4096de864d5fcbcaad1cce6a26
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2024-05-06 15:25:04 +0200

    user orders layout changes

--- app/src/main/java/com/example/restaurantapp/ui/screens/UserOrdersScreen.kt
+++ app/src/main/java/com/example/restaurantapp/ui/screens/UserOrdersScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.restaurantapp.R
 
@@ -31,6 +32,13 @@ fun UserOrdersScreen(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
     ){
+        Text(
+            text = stringResource(R.string.your_orders_heading),
+            modifier = Modifier
+                .fillMaxWidth(),
+            style = MaterialTheme.typography.headlineLarge,
+            textAlign = TextAlign.Center
+        )
         when(userOrdersState){
             is UserOrdersState.Loading -> Text(stringResource(R.string.loading))
             is UserOrdersState.Error -> Text(stringResource(R.string.error))
@@ -39,59 +47,57 @@ fun UserOrdersScreen(
                     modifier = Modifier
                         .widthIn(max = 350.dp)
                 ) {
-                    Text(
-                        text = stringResource(R.string.your_orders_heading),
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        style = MaterialTheme.typography.headlineLarge,
-                    )
                     var orders = userOrdersState.orders
-                    for (order in orders) {
-                        Card (
-                            modifier = Modifier
-                                .padding(4.dp)
-                                .wrapContentHeight()
-                                .defaultMinSize(350.dp, 100.dp)
-                                .widthIn(max = 350.dp)
-
-                        ){
-                            Column(
+                    if (orders.isEmpty()){
+                        Text(text = "No orders")
+                    }else {
+                        for (order in orders) {
+                            Card (
                                 modifier = Modifier
-                                    .padding(8.dp)
-                            ) {
-                                Text(stringResource(R.string.dishes))
-                                order.dishModels.forEach { dish ->
-                                    Text(text = dish.name)
-                                }
-                                var orderStatusMessage = ""
-                                when (order.status){
-                                    1 -> orderStatusMessage =
-                                        stringResource(R.string.order_status_ready)
-                                    2 -> orderStatusMessage =
-                                        stringResource(R.string.order_status_being_prepared)
-                                    3 -> orderStatusMessage =
-                                        stringResource(R.string.order_status_ready_to_pay)
-                                    4 -> orderStatusMessage =
-                                        stringResource(R.string.order_status_paid)
-                                }
-                                Text(stringResource(R.string.table_number) +order.tableModelId)
-                                Text(stringResource(R.string.status) + orderStatusMessage)
-                                Text(stringResource(R.string.price) +order.price)
-                                Row(
-                                    horizontalArrangement = Arrangement.Center,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                ) {
-                                    Button(
-                                        onClick = {
-                                            onSetReadyToPay(order.id)
-                                        },
-                                        enabled = order.status == 1
-                                    ) {
-                                        Text(text = stringResource(R.string.ready_to_pay_button))
-                                    }
-                                }
+                                    .padding(4.dp)
+                                    .wrapContentHeight()
+                                    .defaultMinSize(350.dp, 100.dp)
+                                    .widthIn(max = 350.dp)
 
+                            ){
+                                Column(
+                                    modifier = Modifier
+                                        .padding(8.dp)
+                                ) {
+                                    Text(stringResource(R.string.dishes))
+                                    order.dishModels.forEach { dish ->
+                                        Text(text = dish.name)
+                                    }
+                                    var orderStatusMessage = ""
+                                    when (order.status){
+                                        1 -> orderStatusMessage =
+                                            stringResource(R.string.order_status_ready)
+                                        2 -> orderStatusMessage =
+                                            stringResource(R.string.order_status_being_prepared)
+                                        3 -> orderStatusMessage =
+                                            stringResource(R.string.order_status_ready_to_pay)
+                                        4 -> orderStatusMessage =
+                                            stringResource(R.string.order_status_paid)
+                                    }
+                                    Text(stringResource(R.string.table_number) +order.tableModelId)
+                                    Text(stringResource(R.string.status) + orderStatusMessage)
+                                    Text(stringResource(R.string.price) +order.price)
+                                    Row(
+                                        horizontalArrangement = Arrangement.Center,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                    ) {
+                                        Button(
+                                            onClick = {
+                                                onSetReadyToPay(order.id)
+                                            },
+                                            enabled = order.status == 1
+                                        ) {
+                                            Text(text = stringResource(R.string.ready_to_pay_button))
+                                        }
+                                    }
+
+                                }
                             }
                         }
                     }
